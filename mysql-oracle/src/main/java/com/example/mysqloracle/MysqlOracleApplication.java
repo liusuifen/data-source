cca600dfcd9d923d0a2c5f98109d29960b02c032
liusuifen <2660444092@qq.com>
2020-04-15 13:59:30 +0800

    第一次提交

---
+++ mysql-oracle/src/main/java/com/example/mysqloracle/MysqlOracleApplication.java
@@ -0,0 +1,13 @@
+package com.example.mysqloracle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlOracleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysqlOracleApplication.class, args);
+    }
+
+}
---
+++ mysql-oracle/src/test/java/com/example/mysqloracle/MysqlOracleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mysqloracle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlOracleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
